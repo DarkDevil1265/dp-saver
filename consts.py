77a94820b65adc24f7662a7b6e5dba78d026da07
DarkDevil1265 <90670797+DarkDevil1265@users.noreply.github.com>
2021-10-28 09:41:31 +0400

    Update consts.py

--- consts.py
+++ consts.py
@@ -3,7 +3,7 @@ from telegram.utils.helpers import escape_markdown as es
 
 
 def welcome_msg():
-    welcome_msg = '''Hello f"My name is {(await bot.get_me()).mention}"  😊
+    welcome_msg = '''Hello f"My name is {mention}"  😊
 I am a DP saver bot
 Send me anyones instagram username or profile url to get their DP
 
