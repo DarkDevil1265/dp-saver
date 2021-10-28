1dbf2519865c6cb52a8d8e70e85d277ffef15af8
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-10-28 11:27:29 +0530

    Fix

--- consts.py
+++ consts.py
@@ -3,7 +3,7 @@ from telegram.utils.helpers import escape_markdown as es
 
 
 def welcome_msg():
-    welcome_msg = '''f"""Hello {update.from_user.mention}, My name is {(await bot.get_me()).mention}.""" 😊
+    welcome_msg = f'''Hello, 😊
 I am a DP saver bot
 Send me anyones instagram username or profile url to get their DP
 
