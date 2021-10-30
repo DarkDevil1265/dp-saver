e725a4eeae19ba2d51eec08a3ac5b80750482994
Darkdevil <90670797+DarkDevil1265@users.noreply.github.com>
2021-10-30 14:39:34 +0400

    Update consts.py

--- consts.py
+++ consts.py
@@ -3,7 +3,7 @@ from telegram.utils.helpers import escape_markdown as es
 
 
 def welcome_msg():
-    welcome_msg = '''https://te.legra.ph/file/01080af21db410f8b2d33.jpg
+    welcome_msg = '''![This is an image](https://te.legra.ph/file/01080af21db410f8b2d33.jpg)
 Hello 
 I am a DP saver bot
 Send me anyones instagram username or profile url to get their DP
